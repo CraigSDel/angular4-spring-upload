1d345224712804218f5a8bf3c1c77a5b39c47ca7
Craig Stroberg <craig@psybergate.co.za>
2017-09-15 10:15:56 +0200

    Angular 4 Spring File Upload Working

---
+++ src/main/java/com/angular4spring/Angular4Application.java
@@ -0,0 +1,27 @@
+package com.angular4spring;
+
+import com.angular4spring.service.StorageService;
+import com.angular4spring.storage.StorageProperties;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class Angular4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Angular4Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+
+}
---
+++ src/main/java/com/angular4spring/exceptions/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.angular4spring.exceptions;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
